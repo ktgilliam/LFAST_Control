ab2ea30ea5a80a799caada9f909973060725cc4a
Kevin Gilliam <keving@arizona.edu>
2022-09-01 11:56:43 -0700

    added comms folder and unit test

---
+++ 00_LFAST_Utils/karbon_can_bus.cpp
@@ -0,0 +1,7 @@
+
+#include "karbon_can_bus.h"
+
+void KarbonCANBus::PrintHelloWorld()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ 00_LFAST_Utils/karbon_can_bus.h
@@ -0,0 +1,14 @@
+/** \file lfast_mount_driver.h
+    \brief LFAST Az/El Pedestal INDI Driver
+    \author Kevin Gilliam
+*/
+
+#pragma once
+
+#include <iostream>
+
+class KarbonCANBus
+{
+    public:
+        void PrintHelloWorld();
+};
