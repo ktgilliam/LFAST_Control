f51d5a54956cb10bd2618bde9ea4e52b7b075453
Kevin Gilliam <keving@arizona.edu>
2022-09-10 19:04:23 -0700

    cleanup

--- test/lfast_utils_tests.cpp
+++ test/lfast_utils_tests.cpp
@@ -36,29 +36,6 @@ TEST(karbon_can_bus_tests, kcb_cnstrctr1)
 /// Bash wrapper unit tests ///
 ///
 
-// TEST(bash_wrapper_tests, testExecBashCommand)
-// {
-//     std::stringstream ss;
-//     const std::string bashbw = "echo ";
-//     const std::string dblQuote = "\"";
-//     std::string message = "This is a test.";
-
-//     ss << bashbw << dblQuote << message << dblQuote; // << std::endl;
-//     std::string bashCommandStr = ss.str();
-//     ss.str(""); // clear the stringstream
-
-//     ss << message << std::endl;
-//     std::string expectedResponseStr = ss.str();
-//     ss.str(""); // clear the stringstream
-
-//     std::string receivedResponseStr;
-//     int bashExitCode;
-//     BashCommandWrapper bw;
-//     receivedResponseStr = bw.execBashCommand(bashCommandStr, bashExitCode);
-
-//     EXPECT_STREQ(expectedResponseStr.c_str(), receivedResponseStr.c_str());
-// }
-
 TEST(bash_wrapper_tests, testBashCommandWithPipes)
 {
     BashCommandWrapper bw;
@@ -71,41 +48,3 @@ TEST(bash_wrapper_tests, testBashCommandWithPipes)
     EXPECT_EQ(1, found);
 }
 
-// TEST(bash_wrapper_tests, testBashWrapperSingleLineRead)
-// {
-//     // BashCommandWrapper bw;
-//     // bw.StdIn = "seq 0 255";
-//     // bw.Command = R"(while read n; do printf "%04X;" $n; done)";
-
-//     // int lines = bw.execBashCommandWithPipes_LBL();
-
-//     // std::cout << "#####  RECEIVED " << std::dec << lines << " LINES ######" << std::endl;
-
-//     FAIL();
-// }
-
-// TEST(bash_wrapper_tests, testExecMultiLineStdOut)
-// {
-
-//     std::string bwStr = "seq 0 255";
-
-//     // seq 0 255 | while read n; do printf "%04X;" $n; done
-
-//     // bw.Command = "for ii in {1..5}; do echo $ii && sleep 0.1; done";
-//     // bw.StdIn = "seq 0 5";
-//     // bw.Command = R"(while read -e n; do printf "%04X" $n; done)";
-//     // bw.execBashCommandWithPipes();
-
-//     // std::cout << "REPLY LENGTH: " << bw.StdOut.length() <<std::endl;
-//     // std::cout << "RECEIVED REPLY: " << bw.StdOut << std::endl;
-
-//     // std::string receivedResponseStr;
-//     // int bashExitCode;
-//     // BashCommandWrapper bw;
-//     // receivedResponseStr = bw.execBashCommand(bwStr, bashExitCode);
-//     // std::cout << receivedResponseStr << "################" << std::endl;
-
-//     BashCommandWrapper::CommandWithPipes bw;
-
-//     FAIL();
-// }
