848dff15ec4d51d3ce410436b0cda296937edd89
Kevin Gilliam <keving@arizona.edu>
2023-06-29 15:00:44 -0700

    renamed lfast mount driver

--- 01_Mount_Driver/indi_lfast_mount.xml.cmake
+++ 01_Mount_Driver/indi_lfast_mount.xml.cmake
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <driversList>
    <devGroup group="Telescopes">
-      <device label="LFAST Mount Driver" manufacturer="Steward Observatory ETS">
-         <driver name="LFAST Mount Driver">lfast_mount_driver</driver>
+      <device label="LFAST Mount" manufacturer="Steward Observatory ETS">
+         <driver name="LFAST Mount">indi_lfast_mount</driver>
          <version>@CDRIVER_VERSION_MAJOR@.@CDRIVER_VERSION_MINOR@</version>
       </device>
    </devGroup>
